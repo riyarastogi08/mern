aa2f776ee96951709445f80cd7fd8ee2c8b1d6e8
riyarastogi08 <riyarastogi8892@gmail.com>
2024-04-04 09:39:19 +0530

    signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useFormik} from 'formik'
 import * as Yup from 'yup'
+import { enqueueSnackbar } from 'notistack'
 
 const SignupSchema = Yup.object().shape({
   name: Yup.string()
@@ -27,6 +28,7 @@ const Signup = () => {
     // step 5: validation schema
     onSubmit: (values, {resetForm}) => {
       console.log(values)
+      enqueueSnackbar('Signup successfully', {variant: 'success'})
         resetForm()
     },
     validationSchema: SignupSchema
