cae2d7e3c8020614259c4b0357370763f4b55e3b
riyarastogi08 <riyarastogi8892@gmail.com>
2024-04-08 09:52:55 +0530

    connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const url =''
+const url ='mongodb+srv://<username>:riya8902@riyarastogi.zcrmwq8.mongodb.net/Mern?retryWrites=true&w=majority&appName=riyarastogi'
 mongoose.connect(url)
 
 .then((result) => {
-    console.log('Connected to nthe database')
+    console.log('Connected to the database')
 }).catch((err) => {
     console.log(err)
 });
